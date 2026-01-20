4739372e32888d1a498a3e206d69eb0eed3c0003
hcrawford-tutorials <hcrawford@duck.com>
2026-01-20 12:13:23 -0500

    Update run-exercises.py

--- run-exercises.py
+++ run-exercises.py
@@ -1,3 +1,2 @@
-if __name__ == "__main__":
-    my_portfolio = portfolio.create_portfolio("Retirement")
-    portfolio.print_report(my_portfolio)
+my_portfolio = portfolio.create_portfolio("Retirement")
+portfolio.print_report(my_portfolio)
